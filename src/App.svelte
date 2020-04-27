ea44b930d5f43c217e43b8067e8608d4f0ad6e52
Cornel <filipcornelcristian@gmail.com>
2020-04-27 13:54:34 +0300

    create CustomInput and Toggle components (understanding custom component bindings)

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,12 @@
 <script>
+  import CustomInput from "./CustomInput.svelte";
+  import Toggle from "./Toggle.svelte";
+
   let val = "Maracana";
+  let selectedOption = 1;
 
   $: console.log(val);
+  $: console.log(selectedOption);
 
   const setValue = event => {
     val = event.target.value;
@@ -9,4 +14,6 @@
 </script>
 
 <!-- <input type="text" value={val} on:input={setValue} /> -->
-<input type="text" bind:value={val} />
+<!-- <input type="text" bind:value={val} /> -->
+<CustomInput bind:val />
+<Toggle bind:chosenOption={selectedOption} />
