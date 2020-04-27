e2300fe37ac494b0e5e4f9a92a70bb2929bf5c63
Cornel <filipcornelcristian@gmail.com>
2020-04-27 14:14:20 +0300

    automatic number conversion

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,11 @@
 
   let val = "Maracana";
   let selectedOption = 1;
+  let price = 0;
 
   $: console.log(val);
   $: console.log(selectedOption);
+  $: console.log(price);
 
   const setValue = event => {
     val = event.target.value;
@@ -17,3 +19,12 @@
 <!-- <input type="text" bind:value={val} /> -->
 <CustomInput bind:val />
 <Toggle bind:chosenOption={selectedOption} />
+
+<!-- this will always return a stri ng even if the type is number because this is the default behavior in the DOM and JS -->
+<!-- <input
+  type="number"
+  value={price}
+  on:input={event => console.log(event.target.value)} /> -->
+
+<!-- Svelte actualy helps you with conversion if you use the two-way binding  bind directive -->
+<input type="number" bind:value={price} />
