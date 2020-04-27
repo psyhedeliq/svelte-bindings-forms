c04399d8521efffcc0402b49637ca5fc74c71445
Cornel <filipcornelcristian@gmail.com>
2020-04-27 14:35:49 +0300

    binding to checkboxes and radio buttons

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,15 @@
   let val = "Maracana";
   let selectedOption = 1;
   let price = 0;
+  let agreed;
+  // let favColor = "green";
+  let favColor = ["green"];
 
   $: console.log(val);
   $: console.log(selectedOption);
   $: console.log(price);
+  $: console.log(agreed);
+  $: console.log(favColor);
 
   const setValue = event => {
     val = event.target.value;
@@ -28,3 +33,35 @@
 
 <!-- Svelte actualy helps you with conversion if you use the two-way binding  bind directive -->
 <input type="number" bind:value={price} />
+
+<label>
+  <input type="checkbox" bind:checked={agreed} />
+  Agree to terms?
+</label>
+
+<h1>Favorite color?</h1>
+<!-- <label>
+  <input type="radio" name="color" value="red" bind:group={favColor} />
+  Red
+</label>
+<label>
+  <input type="radio" name="color" value="green" bind:group={favColor} />
+  Green
+</label>
+<label>
+  <input type="radio" name="color" value="blue" bind:group={favColor} />
+  Blue
+</label> -->
+
+<label>
+  <input type="checkbox" name="color" value="red" bind:group={favColor} />
+  Red
+</label>
+<label>
+  <input type="checkbox" name="color" value="green" bind:group={favColor} />
+  Green
+</label>
+<label>
+  <input type="checkbox" name="color" value="blue" bind:group={favColor} />
+  Blue
+</label>
